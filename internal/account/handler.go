7b4edeb8e3cca9d19e9ca9770db5f6b847be81c8
Travis Johnson <mynameismyname1992@gmail.com>
2026-09-24 09:03:55 -0500

    Add account_accessed log. Update NormalizeEmail to lowercase + remove whitespace

--- internal/account/handler.go
+++ internal/account/handler.go
@@ -61,6 +61,14 @@ func (handler *Handler) Page(responseWriter http.ResponseWriter, request *http.R
 	if !ok {
 		return
 	}
+
+	fields := map[string]any{
+		"userId":    current.User.ID,
+		"email":     current.User.Email,
+		"expiresAt": current.Session.ExpiresAt,
+	}
+	_ = handler.logger.Event("account_accessed", fields)
+
 	if err := handler.renderPage(responseWriter, http.StatusOK, current, ""); err != nil {
 		handler.internalError(responseWriter, request, err)
 	}
