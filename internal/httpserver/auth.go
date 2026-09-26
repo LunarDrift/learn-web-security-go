eb69800f89017c323310ece47f85f0befeea45da
Travis Johnson <mynameismyname1992@gmail.com>
2026-09-26 07:54:04 -0500

    Update logout to revoke current session
    Update Store.CurrentSession - revoked sessions are invalid
    Update authHandler.Logout - revoke session if exists

--- internal/httpserver/auth.go
+++ internal/httpserver/auth.go
@@ -233,6 +233,17 @@ func parseForm(_ int64, renderer *templates.Renderer) middleware {
 }
 
 func (handler *authHandler) Logout(responseWriter http.ResponseWriter, request *http.Request) {
+	currentSession, ok, err := sessions.Current(request, handler.accounts)
+	if err != nil {
+		handler.internalError(responseWriter, request, err)
+		return
+	}
+	if ok {
+		if err := handler.accounts.RevokeSession(request.Context(), currentSession.Session.Token); err != nil {
+			handler.internalError(responseWriter, request, err)
+			return
+		}
+	}
 	challengeToken := totpLoginChallengeToken(request)
 	if err := handler.mfa.DeleteChallenge(request.Context(), challengeToken); err != nil {
 		handler.internalError(responseWriter, request, err)
